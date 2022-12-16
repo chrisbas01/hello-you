089fb0e8ff7210e969fd78e49fb5bbd58004f05e
Chris Bascom <bascomchris@gmail.com>
2022-12-16 11:29:07 -0800

    clean up js and link

--- app.js
+++ app.js
@@ -3,17 +3,31 @@ function greetUser(){
     if(name == ""){ 
     name = prompt("What's a matter you don't remember your name");
     }
-        document.write(name +  ", did you know? ");
+    document.write(name +  ", did you know?  ");
+    
+    return name;
 }
-greetUser();
+let userName =  greetUser();
    
     function userAge(){
         let age = prompt("How old are you?");
         if(age < 30){
-          document.write("Get out of the house once in awhile");
+        
+          document.write("..Get out of the house once in awhile... ");
         } else {
-          window.location.href = "http://www.google.com";
+          window.location.href = "https://jaseadamsdev.github.io/hello-travel";
           
         }
       }
       userAge(); 
+
+
+      function Stars(){
+      let output = '';
+      let happy = prompt("Pick a number! (Between 1 & 5 please)");
+       for(let i = 0; i < happy; i++){
+    output += "<img class='happy'src='Stars.jpeg'/>";
+}
+  return document.write(output);
+}
+
