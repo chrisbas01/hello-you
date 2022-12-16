17028c7b86dbd659a4ace027d780410dfe6fd976
Chris Bascom <bascomchris@gmail.com>
2022-12-15 18:07:55 -0800

    java update 2

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 function greetUser(){
     let name = prompt("What shall I call you?");
     if(name == ""){ 
-     name = prompt("What's a matter you don't remember your name");
+    name = prompt("What's a matter you don't remember your name");
     }
-        document.write(name   + ", did you know? ");
+        document.write(name +  ", did you know? ");
 }
 greetUser();
-    // function Second(){
-    //     document.write(name + ", what did you learn?")
-    //     greetUser()
-    //     second()
+   
+    function userAge(){
+        let age = prompt("How old are you?");
+        if(age < 30){
+          document.write("Get out of the house once in awhile");
+        } else {
+          window.location.href = "http://www.google.com";
+          
+        }
+      }
+      userAge(); 
